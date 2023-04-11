d4c4cb221dbad3aed61103c1dfe646d35f8e9bba
Christofer Toledo Luna <ttchristofer@gmail.com>
2023-04-10 23:21:34 -0400

    asdsad

--- src/xls_scraper/excelinteractor.py
+++ src/xls_scraper/excelinteractor.py
@@ -19,7 +19,7 @@ class ExcelInteract:
 
     def __create_temp_copy(self):
         tmp = tempfile.NamedTemporaryFile(delete=False)
-        shutil.copy2(self.file_name, tmp)
+        shutil.copy2(self.file_name, tmp.name)
         return tmp
     
     def __col_title_to_number(self, col_title: str)->int:
@@ -28,11 +28,10 @@ class ExcelInteract:
             ans = ans * 26 + ord(i) - 64
         return ans
 
-    def __parse_cel_name(self, cel_name: str)->tuple[str,int]:
+    def parse_cel_name(self, cel_name: str)->tuple[str,int]:
         c = cel_name.rstrip('0123456789')
         r = cel_name[len(c):]
-        return c, r
-
+        return int(r), self.__col_title_to_number(c)
     def value_from_worksheet(self, wb):
         ws=wb.Worksheets(1)
 
@@ -44,6 +43,7 @@ class ExcelInteract:
             workbook = self.__excel.Workbooks.Open(tmp_file.name)
             yield workbook
         finally:
+            tmp_file.close()
             if workbook:
                 workbook.Close(SaveChanges=False)
 
@@ -55,9 +55,11 @@ with eint.open() as wb:
     ws = wb.Worksheets("Hoja1")
     sheet_names = [sheet.Name for sheet in wb.Sheets]
     print(sheet_names)
-    print(ws.Cells(1,1).Value)
+    ws.Cells(*eint.parse_cel_name("A2")).Value = 10
+    ws.Range("A2:C2").Value = [5,6,3]
     print(list(map(list, ws.Range("A1:E5").Value)))
     print(pd.DataFrame(ws.Range("A1:E5").Value))
+    ""
 
     
 
